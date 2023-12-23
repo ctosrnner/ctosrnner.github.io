26540df5b9756bf970402b7c78408fb69d6684e6
Aldian Fernanda <zortagundul@gmail.com>
2023-12-23 08:51:38 +0700

    Fix #8 spamming on mobile device separate location found event

--- public/js/main.js
+++ public/js/main.js
@@ -60,107 +60,108 @@ fetch(JSON_URL).then(response => {
     let normalMarker = new mainMarker(
       { iconUrl: './../public/img/icon/dmarker_normal.png' })
 
+    let userLocation
     WORLD_MAP.on('locationfound', (e) => {
-        console.log('LOC FOUND')
-        let userLocation = [e.latlng.lat, e.latlng.lng]
+        userLocation = [e.latlng.lat, e.latlng.lng]
+        return userLocation
+    })
 
-        for (const marker in markers) {
-            if (markers[marker].active) {
+    for (const marker in markers) {
+        if (markers[marker].active) {
 
-                const markerStatus = markers[marker]['status']
-                const markerPayout = markers[marker]['payout']
+            const markerStatus = markers[marker]['status']
+            const markerPayout = markers[marker]['payout']
 
-                const userDistance = (WORLD_MAP.distance(userLocation,
-                  markers[marker].coords) / 1000).toFixed(2)
+            console.log(userLocation)
+            const userDistance = (WORLD_MAP.distance([0, 0],
+              markers[marker].coords) / 1000).toFixed(2)
 
-                /*
-                 * Pop-up Message builder
-                 */
-                const popupMessage = `<span class="popup-title">Scrap Information (#${marker})</span>` +
-                  `<span>Distance: ${userDistance}km</span>` +
-                  `<span class="popup-status ${markerStatus
-                    ? 'avn'
-                    : 'unv'}">${markerStatus
-                    ? 'Available'
-                    : 'Unavailable'} <span class="popup-payout ${markerStatus
-                    ? ''
-                    : 'unv'}">IDR ${markerPayout}</span></span>` +
-                  `<div><button id="${markerStatus
-                    ? 'fetch_enable_btn'
-                    : ''}" type="button" class="popup-button-fetch ${markerStatus
-                    ? ''
-                    : 'unv'}">Fetch</button></div>` +
-                  `<span class="latlng-info hidden">${markers[marker].coords[0]}|${markers[marker].coords[1]}</span>`
+            /*
+             * Pop-up Message builder
+             */
+            const popupMessage = `<span class="popup-title">Scrap Information (#${marker})</span>` +
+              `<span>Distance: ${userDistance}km</span>` +
+              `<span class="popup-status ${markerStatus
+                ? 'avn'
+                : 'unv'}">${markerStatus
+                ? 'Available'
+                : 'Unavailable'} <span class="popup-payout ${markerStatus
+                ? ''
+                : 'unv'}">IDR ${markerPayout}</span></span>` +
+              `<div><button id="${markerStatus
+                ? 'fetch_enable_btn'
+                : ''}" type="button" class="popup-button-fetch ${markerStatus
+                ? ''
+                : 'unv'}">Fetch</button></div>` +
+              `<span class="latlng-info hidden">${markers[marker].coords[0]}|${markers[marker].coords[1]}</span>`
 
-                L.polygon(getCoordsBounding(markers[marker].coords[0],
-                  markers[marker].coords[1], config['boundingExpand'])).
-                  addTo(WORLD_MAP)
+            L.polygon(getCoordsBounding(markers[marker].coords[0],
+              markers[marker].coords[1], config['boundingExpand'])).
+              addTo(WORLD_MAP)
 
-                L.marker(markers[marker].coords,
-                  { icon: markerStatus ? normalMarker : warnMarker }).
-                  addTo(WORLD_MAP).
-                  bindPopup(popupMessage)
+            L.marker(markers[marker].coords,
+              { icon: markerStatus ? normalMarker : warnMarker }).
+              addTo(WORLD_MAP).
+              bindPopup(popupMessage)
 
-                let { boundingExpand } = config
+            let { boundingExpand } = config
 
-                L.circle(markers[marker].coords, {
-                    color: markerStatus ? '#3b73cc' : '#db3e3e',
-                    fillColor: markerStatus ? '#3b73cc' : '#db3e3e',
-                    fillOpacity: 0.35,
-                    radius: (boundingExpand * 112500),
-                }).addTo(WORLD_MAP)
-            }
-
+            L.circle(markers[marker].coords, {
+                color: markerStatus ? '#3b73cc' : '#db3e3e',
+                fillColor: markerStatus ? '#3b73cc' : '#db3e3e',
+                fillOpacity: 0.35,
+                radius: (boundingExpand * 112500),
+            }).addTo(WORLD_MAP)
         }
 
-        let lastFetchedAction = 0
+    }
 
-        function handleFetchButton (lat, lng) {
-            const currentTime = new Date().getTime()
-            if (currentTime - lastFetchedAction > 1000) {
+    let lastFetchedAction = 0
 
-                window.open(
-                  `pages/fetch.html?lat=${lat}&lng=${lng}&b=${config['boundingExpand']}`,
-                  '_blank')
+    function handleFetchButton (lat, lng) {
+        const currentTime = new Date().getTime()
+        if (currentTime - lastFetchedAction > 1000) {
 
-                lastFetchedAction = currentTime
-            }
+            window.open(
+              `pages/fetch.html?lat=${lat}&lng=${lng}&b=${config['boundingExpand']}`,
+              '_blank')
+
+            lastFetchedAction = currentTime
         }
+    }
 
-        /*
-         * Handling Popup Event
-         */
-        WORLD_MAP.on('popupopen', () => {
-            setTimeout(function () {
-                let fetchButton = document.querySelector('#fetch_enable_btn')
-                if (fetchButton) {
-                    let coords = document.querySelector('.latlng-info').
-                      innerHTML.
-                      split('|')
+    /*
+     * Handling Popup Event
+     */
+    WORLD_MAP.on('popupopen', () => {
+        setTimeout(function () {
+            let fetchButton = document.querySelector('#fetch_enable_btn')
+            if (fetchButton) {
+                let coords = document.querySelector('.latlng-info').
+                  innerHTML.
+                  split('|')
 
-                    coords = [parseFloat(coords[0]), parseFloat(coords[1])]
+                coords = [parseFloat(coords[0]), parseFloat(coords[1])]
 
-                    fetchButton.addEventListener('click', (event) => {
-                        event.preventDefault()
+                fetchButton.addEventListener('click', function (event) {
+                    event.preventDefault()
+                    handleFetchButton(coords[0], coords[1])
+                    // window.open(
+                    //   `pages/fetch.html?lat=${coords[0]}&lng=${coords[1]}&b=${config['boundingExpand']}`,
+                    //   '_blank')
+                })
+            }
+        }, 100)
 
-                        handleFetchButton(coords[0], coords[1])
-                        // window.open(
-                        //   `pages/fetch.html?lat=${coords[0]}&lng=${coords[1]}&b=${config['boundingExpand']}`,
-                        //   '_blank')
-                    })
-                }
-            }, 100)
+        // L.marker(defaultCoords, { icon: normalMarker }).addTo(WORLD_MAP)
+
+        WORLD_MAP.on('locationerror', (e) => {
+            console.log('Error saat mendapatkan lokasi:', e.message)
         })
+
+    }).catch(error => {
+        console.error('Error loading JSON:', error)
     })
-
-    // L.marker(defaultCoords, { icon: normalMarker }).addTo(WORLD_MAP)
-
-    WORLD_MAP.on('locationerror', (e) => {
-        console.log('Error saat mendapatkan lokasi:', e.message)
-    })
-
-}).catch(error => {
-    console.error('Error loading JSON:', error)
 })
 
 
